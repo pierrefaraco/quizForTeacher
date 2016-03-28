e3cbc8d73c33743addafbb91f4670932f66efeba
pierrot5526 <pierre.faraco@gmail.com>
2016-03-28 12:02:32 +0200

    second

--- src/main/java/com/pfaraco/App.java
+++ src/main/java/com/pfaraco/App.java
@@ -8,7 +8,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		switch ("3") {
-		case "3":System.out.println("Hello World 2!");
+		case "3":System.out.println("Hello World 6!");
 			break;
 		}
 	}
