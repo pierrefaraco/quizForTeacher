209b1e5ba0b09fefcf17624cb198971603acb2c1
pierrot5526 <pierre.faraco@gmail.com>
2016-03-28 11:53:57 +0200

    first commit

---
+++ src/main/java/com/pfaraco/App.java
@@ -0,0 +1,15 @@
+package com.pfaraco;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		switch ("3") {
+		case "3":System.out.println("Hello World 2!");
+			break;
+		}
+	}
+}
