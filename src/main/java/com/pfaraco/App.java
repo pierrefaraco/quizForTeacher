07599163a64df005b9dbea083d06eca8a53447dc
pierrot5526 <pierre.faraco@gmail.com>
2016-03-28 12:10:17 +0200

    fourth

--- src/main/java/com/pfaraco/App.java
+++ src/main/java/com/pfaraco/App.java
@@ -8,7 +8,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		switch ("3") {
-		case "3":System.out.println("Hello World 3!");
+		case "3":System.out.println("Hello World 4!");
 			break;
 		}
 	}
