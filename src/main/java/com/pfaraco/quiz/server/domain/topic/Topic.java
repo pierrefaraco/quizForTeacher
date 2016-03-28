93470cafcc8dd13f3ba6f18ed70a091052305653
pierrot5526 <pierre.faraco@gmail.com>
2016-03-28 12:29:36 +0200

    My first commit

--- src/main/java/com/pfaraco/quiz/server/domain/topic/Topic.java
+++ src/main/java/com/pfaraco/quiz/server/domain/topic/Topic.java
@@ -1,11 +1,53 @@
 package com.pfaraco.quiz.server.domain.topic;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "TOPICS")
 public class Topic {	
+	
 	private String id;
 	private String name;
 	private String description;
 	
+	public Topic() {
+		
+	}
 	
+	public Topic(String name, String description) {
+		super();
+		this.name = name;
+		this.description = description;
+	}
 	
-
+	public Topic(String id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
