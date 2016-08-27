84f356b0b0ef3e80c06e0f54d0634d7e306bee1f
pierr <pierr@192.168.1.13>
2016-08-27 10:41:24 +0200

    renommage des packages com.cnam => cnam.gl204

---
+++ src/main/java/cnam/glg204/quiz/common/dto/CoursWithSubscribersDto.java
@@ -0,0 +1,105 @@
+package cnam.glg204.quiz.common.dto;
+
+import cnam.glg204.quiz.common.dto.jsonserial.CoursWithSubscribersDtoDeserializer;
+import cnam.glg204.quiz.common.dto.jsonserial.CoursWithSubscribersDtoSerializer;
+import java.util.HashMap;
+import java.util.Map;
+
+import cnam.glg204.quiz.common.enums.SubscriberStatus;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize(using = CoursWithSubscribersDtoSerializer.class)
+@JsonDeserialize(using = CoursWithSubscribersDtoDeserializer.class)
+public class CoursWithSubscribersDto {
+	private long id;
+	private String name;
+	private String description;
+	boolean active;
+	private long userId;
+	private HashMap <UserDto,SubscriberStatus> subscribers;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public Map <UserDto, SubscriberStatus> getSubscribers() {
+		return subscribers;
+	}
+	public void setSubscribers(HashMap<UserDto, SubscriberStatus> subscribers) {
+		this.subscribers = subscribers;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (active ? 1231 : 1237);
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((subscribers == null) ? 0 : subscribers.hashCode());
+		result = prime * result + (int) (userId ^ (userId >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CoursWithSubscribersDto other = (CoursWithSubscribersDto) obj;
+		if (active != other.active)
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (subscribers == null) {
+			if (other.subscribers != null)
+				return false;
+		} else if (!subscribers.equals(other.subscribers))
+			return false;
+		if (userId != other.userId)
+			return false;
+		return true;
+	}
+
+
+
+}
---
+++ src/main/java/cnam/glg204/quiz/common/dto/UserDto.java
@@ -0,0 +1,130 @@
+package cnam.glg204.quiz.common.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+import cnam.glg204.quiz.common.enums.AccountType;
+
+public class UserDto implements Serializable {
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String birthDay;
+	private String email;
+	private String presentation;
+	private String password;
+	private AccountType accountType;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String  getBirthDay() {
+		return birthDay;
+	}
+	public void setBirthDay(String  birthDay) {
+		this.birthDay = birthDay;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPresentation() {
+		return presentation;
+	}
+	public void setPresentation(String presentation) {
+		this.presentation = presentation;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public AccountType getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(AccountType accountType) {
+		this.accountType = accountType;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((accountType == null) ? 0 : accountType.hashCode());
+		result = prime * result + ((birthDay == null) ? 0 : birthDay.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((presentation == null) ? 0 : presentation.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserDto other = (UserDto) obj;
+		if (accountType != other.accountType)
+			return false;
+		if (birthDay == null) {
+			if (other.birthDay != null)
+				return false;
+		} else if (!birthDay.equals(other.birthDay))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (presentation == null) {
+			if (other.presentation != null)
+				return false;
+		} else if (!presentation.equals(other.presentation))
+			return false;
+		return true;
+	}	
+	
+	
+	
+}
